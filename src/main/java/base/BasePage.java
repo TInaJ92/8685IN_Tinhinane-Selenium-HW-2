beb47477aee14a10442c6cfaaf5c530bbb0f5467
samipiit <sami@piit.us>
2022-04-16 22:59:13 -0400

    Added reporting and screenshot functionality

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,23 +1,49 @@
 package base;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import reporting.ExtentManager;
+import reporting.ExtentTestManager;
 
+import java.io.File;
+import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
 
 public class BasePage {
 
     public static final String URL = "http://amazon.com";
     public static WebDriver driver;
     public static WebDriverWait webDriverWait;
+    public static ExtentReports extent;
+
+    @BeforeSuite(alwaysRun = true)
+    public static void reportSetup(ITestContext context) {
+        ExtentManager.setOutputDirectory(context);
+        extent = ExtentManager.getInstance();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public static void reportInit(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        String methodName = method.getName();
+
+        ExtentTestManager.startTest(methodName);
+        ExtentTestManager.getTest().assignCategory(className);
+    }
 
     @Parameters("browser")
     @BeforeMethod
@@ -35,6 +61,31 @@ public class BasePage {
         driver.quit();
     }
 
+    @AfterMethod(alwaysRun = true)
+    public void afterEachTestMethod(ITestResult result) {
+
+        ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getEndMillis()));
+
+        for (String group : result.getMethod().getGroups()) {
+            ExtentTestManager.getTest().assignCategory(group);
+        }
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            ExtentTestManager.getTest().log(LogStatus.FAIL, "TEST CASE FAILED: " + result.getName());
+            ExtentTestManager.getTest().log(LogStatus.FAIL, result.getThrowable());
+            captureScreenshot(driver, result.getName());
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "TEST CASE SKIPPED: " + result.getName());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "TEST CASE PASSED: " + result.getName());
+        }
+
+        ExtentTestManager.endTest();
+        extent.flush();
+    }
+
+    // region Helper Methods
     public void driverInit(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
@@ -49,4 +100,27 @@ public class BasePage {
 
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
+    private static void captureScreenshot(WebDriver driver, String testName) {
+        String fileName = testName + ".png";
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File newScreenshotFile = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator +
+                "test" + File.separator + "reports" + File.separator + fileName);
+
+        try {
+            FileHandler.copy(screenshot, newScreenshotFile);
+            System.out.println("SCREENSHOT TAKEN");
+        } catch (Exception e) {
+            System.out.println("ERROR TAKING SCREENSHOT: " + e.getMessage());
+        }
+    }
+
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+
+    // endregion
+
 }
---
+++ src/main/java/reporting/ExtentManager.java
@@ -0,0 +1,36 @@
+package reporting;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import org.testng.ITestContext;
+import org.testng.Reporter;
+
+import java.io.File;
+import java.util.Date;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+    private static ITestContext context;
+
+    public synchronized static ExtentReports getInstance(){
+        if(extent == null){
+            File outputDirectory = new File(context.getOutputDirectory());
+            File resultDirectory = new File(outputDirectory.getParentFile(),"html");
+
+            Date date = new Date();
+//            String fileName = "report_" + date.toString().replace(" ", "_").replace(":", "_")+".html";
+            String fileName = "test_report.html";
+            extent = new ExtentReports(System.getProperty("user.dir")+"\\src\\test\\reports\\" + fileName,true);
+            Reporter.log("Extent Report Directory" + resultDirectory, true);
+            extent.addSystemInfo("Host Name", "Maximus").addSystemInfo("Environment","QA")
+                    .addSystemInfo("User Name", "Sami Sheikh");
+
+            extent.loadConfig(new File(System.getProperty("user.dir")+ "/src/main/resources/reporting/report-config.xml"));
+        }
+        return extent;
+    }
+
+    public static void setOutputDirectory(ITestContext context){
+        ExtentManager.context = context;
+    }
+}
